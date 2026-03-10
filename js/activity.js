009926f113a081020fa063f53f512227637319bb
Eric Hall <ewhall18@gmail.com>
2026-03-10 12:26:12 -0400

    color on highlight

--- js/activity.js
+++ js/activity.js
@@ -46,6 +46,10 @@ function id(node) {
     return `${node.parent ? id(node.parent) + "/" : ""}${node.data.name}`;
 }
 
+function id_tooltip(node) {
+  return node.parent.data.name + ': ' + node.data.name;
+}
+
 //rebuild hierarchy data and redraw chart on selection change
 function draw(airlineFilter) {
 
@@ -83,3 +87,71 @@ draw("all"); // initial draw
 // - on hover, highlight the hovered node and all directly connected nodes and links (incoming and outgoing)
 // - on unhover, return all nodes and links to their default state
 // update the tooltip to show the focus airport code and region, as well as number of incoming and outgoing routes
+function createChart(data) {
+  const width = 954;
+  const radius = width / 2;
+
+  const tree = d3.cluster()
+    .size([2 * Math.PI, radius - 100]);
+  const root = tree(bilink(d3.hierarchy(data)
+      .sort((a, b) => d3.ascending(a.height, b.height) || d3.ascending(a.data.name, b.data.name))));
+
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", width)
+      .attr("viewBox", [-width / 2, -width / 2, width, width])
+      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+  const node = svg.append("g")
+    .selectAll()
+    .data(root.leaves())
+    .join("g")
+      .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
+    .append("text")
+      .attr("dy", "0.31em")
+      .attr("x", d => d.x < Math.PI ? 6 : -6)
+      .attr("text-anchor", d => d.x < Math.PI ? "start" : "end")
+      .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
+      .text(d => d.data.name)
+      .each(function(d) { d.text = this; })
+      .on("mouseover", overed)
+      .on("mouseout", outed)
+      .call(text => text.append("title").text(d => `${id_tooltip(d)}
+${d.outgoing.length} outgoing
+${d.incoming.length} incoming`));
+
+  const line = d3.lineRadial()
+    .curve(d3.curveBundle.beta(0.85))
+    .radius(d => d.y)
+    .angle(d => d.x);
+
+  const link = svg.append("g")
+      .attr("stroke", colornone)
+      .attr("fill", "none")
+    .selectAll()
+    .data(root.leaves().flatMap(leaf => leaf.outgoing))
+    .join("path")
+      .style("mix-blend-mode", "multiply")
+      .attr("d", ([i, o]) => line(i.path(o)))
+      .each(function(d) { d.path = this; });
+
+  function overed(event, d) {
+    link.style("mix-blend-mode", null);
+    d3.select(this).attr("font-weight", "bold");
+    d3.selectAll(d.incoming.map(a => a.path)).attr("stroke", (a, i, elements) => airlineColor[a[2]]  ).raise();
+    d3.selectAll(d.incoming.map(([a]) => a.text)).attr("fill", (a, i, elements) => airlineColor[a[2]]).attr("font-weight", "bold");
+    d3.selectAll(d.outgoing.map(a => a.path)).attr("stroke", (a, i, elements) => airlineColor[a[2]]).raise();
+    d3.selectAll(d.outgoing.map(([, a]) => a.text)).attr("fill", (a, i, elements) => airlineColor[a[2]]).attr("font-weight", "bold");
+  }
+
+  function outed(event, d) {
+    link.style("mix-blend-mode", "multiply");
+    d3.select(this).attr("font-weight", null);
+    d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", null);
+    d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", null).attr("font-weight", null);
+    d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", null);
+    d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", null).attr("font-weight", null);
+  }
+
+  return svg.node();
+}
